910061ede47c5bcc11f07499ec912ccde6794eeb
Raghad Abdullah <Raghad.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-12 14:49:22 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+
+
+// مثال باقي القسمه
+const val APPLES = 50 // total apples
+fun main(args: Array<String>) {
+
+    val noOfDozens= APPLES/ 12
+        val remainApples= APPLES % 12
+
+    println(noOfDozens)//50 توزعت و صارت 4
+    println(remainApples)// المتبقي 2
+}
+
+
+
+
+
+
